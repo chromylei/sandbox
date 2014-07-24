b6959033f5a706ed15f2591cf4e91b746c6a51c7
lenxyang <lenxyang@gmail.com>
2014-07-24 09:46:37 +0800

    .

--- base/animation.cc
+++ base/animation.cc
@@ -1,8 +1,45 @@
 #include "sbox/base/animation.h"
 #include "sbox/base/assimp.h"
+#include "sbox/base/skeleton.h"
 
-void Animation::Load(aiScene* scene) {
+void Animation::Load(aiAnimation* anim) {
+  for (uint32 i = 0; i < anim->mNumChannels; ++i) {
+    aiNodeAnim* anim_node = anim->mChannels[i];
+    std::string node_name = std::string(anim_node->mNodeName.data);
+    for (int j = 0; j < anim_node->mNumPositionKeys;  ++j) {
+      aiVectorKey& key = anim_node->mPositionKeys[j];
+      positions_.Add(azer::Vector4(
+          key.mValue.x, key.mValue.y, key.mValue.z, 1.0), key.mTime);
+    }
+
+    for (int j = 0; j < anim_node->mNumScalingKeys;  ++j) {
+      aiVectorKey& key = anim_node->mScalingKeys[j];
+      scales_.Add(azer::Vector4(
+          key.mValue.x, key.mValue.y, key.mValue.z, 1.0), key.mTime);
+    }
+
+    for (int j = 0; j < anim_node->mNumRotationKeys;  ++j) {
+      aiQuatKey& key = anim_node->mRotationKeys[j];
+      rotate_.Add(azer::Quaternion(
+          key.mValue.w, key.mValue.x, key.mValue.y, key.mValue.z), key.mTime);
+    }
+  }
+}
+
+AnimationSet::~AnimationSet() {
+  for (auto iter = animations_.begin(); iter != animations_.end(); ++iter) {
+    delete iter->second;
+  }
+}
+
+void AnimationSet::Load(aiScene* scene) {
   for (uint32 i = 0; i < scene->mNumAnimations; ++i) {
-    aiAnimation* anim = scene[i];
+    aiAnimation* ai_anim = scene->mAnimations[i];
+    std::string name(ai_anim->mName.data);
+    int bone_index = skeleton_->GetBoneIndex(name);
+    Bone* bone = skeleton_->GetBone(bone_index);
+    Animation* anim = new Animation(bone);
+    anim->Load(ai_anim);
+    animations_.insert(std::make_pair(name, anim));
   }
 }
--- base/animation.h
+++ base/animation.h
@@ -6,35 +6,73 @@
 #include "base/basictypes.h"
 #include "sbox/base/skeleton.h"
 #include "azer/math/math.h"
+#include "base/logging.h"
+
+
+template <class T>
+class AnimationData {
+ public:
+  AnimationData() : duration_(0) {}
+  double duration() const { return duration_; }
+  const T& data(double t) const {
+    CHECK(data_.size() > 0);
+    CHECK(duration_ > 0);
+    return data_[FindIndex(t)];
+  }
+
+  void Add(const T& t, double duration) {
+    duration_ += duration_;
+    data_.push_back(t);
+  }
+ private:
+  int FindIndex(double t) const {
+    while (t > duration_) {
+      t -= duration_;
+    }
+
+    for (size_t i = 0; i < time_.size(); ++i) {
+      if (t > *iter) {
+        return i;
+      }
+    }
+    NOTREACHED();
+    return -1;
+  }
+  std::vector<double> time_;
+  std::vector<T> data_;
+  double duration_;
+
+  friend class Animation;
+  DISALLOW_COPY_AND_ASSIGN(AnimationData);
+};
 
 struct aiScene;
+struct aiAnimation;
+struct aiVectorKey;
+struct aiQuatKey;
 class Animation {
  public:
-  Animation() {}
-  void Load(aiScene* scene);
+  explicit Animation(Bone* bone) : bone_(bone) {}
+  void Load(aiAnimation* anim);
 
   const std::string& name() const { return name_;}
-
-  struct AnimationData {
-    azer::Vector4 position;
-    azer::Vector4 scale;
-    azer::Quaternion rotate;
-    Bone* bone;
-  };
-  typedef std::vector<AnimationData> AnimationDataVec;
-  const AnimationDataVec& GetAnimationData() const { return animation_;}
-  AnimationDataVec& GetAnimationData() { return animation_;}
  private:
+  double duration_;
   std::string name_;
-  std::vector<AnimationData> animation_;
+  AnimationData<azer::Vector4> positions_;
+  AnimationData<azer::Vector4> scales_;
+  AnimationData<azer::Quaternion> rotate_;
+  Bone* bone_;
   DISALLOW_COPY_AND_ASSIGN(Animation);
 };
 
 class AnimationSet {
  public:
-  AnimationSet() {}
+  explicit AnimationSet(Skeleton* skeleton) : skeleton_(skeleton) {}
+  ~AnimationSet();
   void Load(aiScene* scene);
  private:
+  Skeleton* skeleton_;
   std::map<std::string, Animation*> animations_;
   DISALLOW_COPY_AND_ASSIGN(AnimationSet);
 };
