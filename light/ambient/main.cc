ac1a75b937aa07b16b5105aa7dd8323115dc155f
yanglei <lenxyang@gmail.com>
2014-07-29 23:24:39 +0800

    .

--- light/ambient/main.cc
+++ light/ambient/main.cc
@@ -16,6 +16,7 @@ using base::FilePath;
 #define kSphereMeshPath "sbox/res/sphere.3DS"
 #define kTeaportMeshPath "sbox/res/teaport.3DS"
 #define kTorusMeshPath "sbox/res/torus.3DS"
+#define kPlaneMeshPath "sbox/res/plane.3DS"
 #define kXYZMeshPath "sbox/res/xyz.x3d"
 
 void Render(AmbientEffect* effect, azer::Renderer* renderer, Mesh* mesh) {
@@ -49,19 +50,16 @@ class MainDelegate : public azer::WindowHost::Delegate {
     renderer->EnableDepthTest(true);
     // renderer->SetCullingMode(azer::kCullNone);
     // renderer->SetFillMode(azer::kWireFrame);
-
-    azer::Vector3 eye(0.0f, 0.0f, 5.0f);
-    azer::Vector3 dir(0.0f, 0.0f, -1.0f);
-    azer::Vector3 up(0.0f, 1.0f, 0.0f);
-    view_ = std::move(LookDirRH(eye, dir, up));
     proj_ = std::move(
         PerspectiveRHD3D(azer::Degree(45.0f), 4.0f / 3.0f, 0.10f, 100.0f));
 
-    camera_.SetPosition(azer::Vector3(0.0f, 0.0f, 5.0f));
+    camera_.SetPosition(azer::Vector3(0.0f, 1.0f, 2.0f));
     LoadMesh(::base::FilePath(::base::UTF8ToWide(kTeaportMeshPath)), &teaport_, rs);
+    LoadMesh(::base::FilePath(::base::UTF8ToWide(kPlaneMeshPath)), &plane_, rs);
 
-    light_.direction = azer::Vector4(0.5f, 0.5f, 0.5f, 1.0f);
-    light_.diffuse = azer::Vector4(0.1f, 1.0f, 1.0f, 1.0f);
+    light_.direction = azer::Vector4(0.5f, -0.5f, 0.0f, 1.0f);
+    light_.diffuse = azer::Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+    light_.ambient = azer::Vector4(0.1f, 0.0f, 0.0f, 1.0f);
   }
 
   virtual void OnUpdateScene(double time, float delta_time) {
@@ -69,8 +67,8 @@ class MainDelegate : public azer::WindowHost::Delegate {
     azer::Radians camera_speed(azer::kPI / 2.0f);
     UpdatedownCamera(&camera_, camera_speed, delta_time);
     azer::Matrix4 scale_mat = azer::Scale(azer::Vector3(0.3f, 0.3f, 0.3f));
-    azer::Matrix4 rotate1 = azer::RotateY(azer::Degree(270.0f));
-    azer::Matrix4 rotate2 = azer::RotateY(azer::Radians(rspeed));
+    azer::Matrix4 rotate1 = azer::RotateX(azer::Degree(90.0f));
+    azer::Matrix4 rotate2 = azer::RotateY(azer::Radians(rspeed * time));
     azer::Matrix4 pos = azer::Translate(azer::Vector3(0.0f, 0.0f, 0.0f));
     teaport_world_ = pos * rotate2 * rotate1 * scale_mat;
   }
@@ -78,25 +76,34 @@ class MainDelegate : public azer::WindowHost::Delegate {
   virtual void OnRenderScene(double time, float delta_time) {
     azer::RenderSystem* rs = azer::RenderSystem::Current();
     azer::Renderer* renderer = rs->GetDefaultRenderer();
-    renderer->SetCullingMode(azer::kCullNone);
+    // renderer->SetCullingMode(azer::kCullNone);
     DCHECK(NULL != rs);
     renderer->Clear(azer::Vector4(0.0f, 0.0f, 0.0f, 1.0f));
     renderer->ClearDepthAndStencil();
 
     azer::Matrix4 pvw;
     pvw = camera_.GetProjViewMatrix() * teaport_world_;
+    effect_->SetPointLight(light_);
+    effect_->SetDiffuse(azer::Vector4(0.4f, 0.4f, 0.4f, 1.0f));
     effect_->SetPVW(pvw);
     effect_->SetWorld(teaport_world_);
     Render(effect_.get(), renderer, &teaport_);
+
+    azer::Matrix4 world = azer::RotateX(azer::Degree(90.0f));
+    pvw = camera_.GetProjViewMatrix() * world;
+    effect_->SetDiffuse(azer::Vector4(0.0f, 1.0f, 0.0f, 1.0f));
+    effect_->SetPVW(pvw);
+    effect_->SetWorld(world);
+    Render(effect_.get(), renderer, &plane_);
   }
 
   virtual void OnQuit() {}
  private:
   azer::Camera camera_;
   azer::Matrix4 proj_;
-  azer::Matrix4 view_;
   azer::Matrix4 teaport_world_;
   Mesh teaport_;
+  Mesh plane_;
   std::unique_ptr<AmbientEffect> effect_;
   AmbientEffect::PointLight light_;
 };
